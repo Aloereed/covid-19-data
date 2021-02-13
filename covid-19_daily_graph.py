2086c1a5d8425e97d71c57498cd5d62f16466dee
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-12 23:12:53 -0500

    update

--- covid-19_daily_graph.py
+++ covid-19_daily_graph.py
@@ -4,15 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dates_csv = np.genfromtxt('/home/nuke/git/covid-19/us_covid-19_dates.csv', dtype='datetime64')
-total_deaths_csv = np.genfromtxt('/home/nuke/git/covid-19/us_covid-19_total_deaths.csv', dtype='int32')
 daily_deaths_csv = np.genfromtxt('/home/nuke/git/covid-19/us_covid-19_daily_deaths.csv', dtype='int32')
 
 x = np.array(dates_csv)
-y = np.array(total_deaths_csv)
 z = np.array(daily_deaths_csv)
 
 plt.figure(figsize=(15, 8))
-plt.title("COVID-19 Monthly Deaths")
+plt.title("COVID-19 Daily Deaths")
 plt.xlabel("Date")
 plt.ylabel("Deaths")
 plt.grid(True, ls = "-.")
--- covid-19_total_graph.py
+++ covid-19_total_graph.py
@@ -5,11 +5,9 @@ import matplotlib.pyplot as plt
 
 dates_csv = np.genfromtxt('/home/nuke/git/covid-19/us_covid-19_dates.csv', dtype='datetime64')
 total_deaths_csv = np.genfromtxt('/home/nuke/git/covid-19/us_covid-19_total_deaths.csv', dtype='int32')
-daily_deaths_csv = np.genfromtxt('/home/nuke/git/covid-19/us_covid-19_daily_deaths.csv', dtype='int32')
 
 x = np.array(dates_csv)
 y = np.array(total_deaths_csv)
-z = np.array(daily_deaths_csv)
 
 plt.figure(figsize=(15, 8))
 plt.title("COVID-19 Total Deaths")
