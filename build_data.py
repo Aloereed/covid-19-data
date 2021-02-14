85ce01302b6b1579fb0c73a44978e9aeecd4b617
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-14 17:32:57 -0500

    update

---
+++ build_data.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import requests
+import pandas as pd
+import io
+import numpy as np
+
+# Fetch data
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+download = requests.get(url).content
+df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+df.to_csv("us.csv", index=False)
+
+# Extract each column individually
+dates = pd.read_csv('/home/nuke/git/covid-19-data/us.csv', usecols=['date'])
+cases_total = pd.read_csv('/home/nuke/git/covid-19-data/us.csv', usecols=['cases'])
+deaths_total = pd.read_csv('/home/nuke/git/covid-19-data/us.csv', usecols=['deaths'])
+
+# Create csv's for extracted columns
+dates.to_csv("us_covid-19_dates.csv", header=False, index=False)
+cases_total.to_csv("us_covid-19_cases_total.csv", header=False, index=False)
+deaths_total.to_csv("us_covid-19_deaths_total.csv", header=False, index=False)
+
+# Calculate the difference of total values to determine daily delta's
+# csv files
+cases_total_csv = np.genfromtxt('/home/nuke/git/covid-19-data/us_covid-19_cases_total.csv', dtype='int32')
+deaths_total_csv = np.genfromtxt('/home/nuke/git/covid-19-data/us_covid-19_deaths_total.csv', dtype='int32')
+    
+# Calculate the difference
+cases_total = np.array(cases_total_csv)
+cases_diff = np.diff(cases_total)
+cases_daily = np.insert(cases_diff, 0, 0)
+deaths_total = np.array(deaths_total_csv)
+deaths_diff = np.diff(deaths_total)
+deaths_daily = np.insert(deaths_diff, 0, 0)
+
+# Create csv
+np.savetxt('us_covid-19_deaths_daily.csv', deaths_daily, fmt='%d')
+np.savetxt('us_covid-19_cases_daily.csv', cases_daily, fmt='%d')
+
+# Aggregate all new data to one file
+# csv files
+df = pd.read_csv('/home/nuke/git/covid-19-data/us.csv')
+cases_daily = pd.read_csv('/home/nuke/git/covid-19-data/us_covid-19_cases_daily.csv', header=None, dtype=str)
+deaths_daily = pd.read_csv('/home/nuke/git/covid-19-data/us_covid-19_deaths_daily.csv', header=None, dtype=str)
+
+# Create csv
+df["daily cases"] = cases_daily
+df["daily deaths"] = deaths_daily
+df.to_csv("us_covid-19_data.csv", index=False)
