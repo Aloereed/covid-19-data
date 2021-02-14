b7e0e7f7a5c24c2eafc539d01b82f08071a2595d
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-14 17:04:06 -0500

    update

--- aggregate.py
+++ aggregate.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
-from fetch import fetch_data
 import pandas as pd
-
-# Fetch Data
-fetch_data()
 
 # .csv files
 df = pd.read_csv('/home/nuke/git/covid-19-data/us.csv')
-cases_daily = pd.read_csv('/home/nuke/git/covid-19-data/us_covid-19_cases_daily.csv', dtype=str)
-deaths_daily = pd.read_csv('/home/nuke/git/covid-19-data/us_covid-19_deaths_daily.csv', dtype=str)
+cases_daily = pd.read_csv('/home/nuke/git/covid-19-data/us_covid-19_cases_daily.csv', header=None, dtype=str)
+deaths_daily = pd.read_csv('/home/nuke/git/covid-19-data/us_covid-19_deaths_daily.csv', header=None, dtype=str)
 
 # Aggregate data
 df["daily cases"] = cases_daily
