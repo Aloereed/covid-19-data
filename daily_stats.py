02941ac9165599f49382b9e4ab7234faeaa43746
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-14 22:48:51 -0500

    update

--- daily_stats.py
+++ daily_stats.py
@@ -5,9 +5,11 @@ import datetime
 import pandas as pd 
 from tabulate import tabulate
 
+# Import csv files
 cases_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_cases_daily.csv', dtype='int32')
 deaths_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_daily.csv', dtype='int32')
 
+# Build arrays
 cases_array = np.array(cases_daily_csv)
 cases_length = len(cases_array)
 cases_today = cases_array[cases_length - 1]
@@ -18,6 +20,7 @@ deaths_length = len(deaths_array)
 deaths_today = deaths_array[deaths_length - 1]
 deaths_mean = np.mean(deaths_array)
 
+# Build DataFrames
 today = datetime.datetime.today().strftime('%m-%d-%Y')
 index_today = ['Cases', 'Deaths']
 df_today = pd.DataFrame({today: [cases_today, deaths_today]}, index=index_today) 
@@ -27,5 +30,7 @@ index_avg = ['Cases per day', 'Deaths per day']
 df_avg = pd.DataFrame({'avg': [int(cases_mean), int(deaths_mean)]}, index=index_avg)
 df_avg.index.names = ['01-21-2020 - ' + today]
 
-print("###### Stats for Today\n" + df_today.to_markdown())
-print("\n\n###### Averages\n" + df_avg.to_markdown())
+# Write to 'daily_stats.md'
+f = open("/home/nuke/git/covid-19-data/data/daily_stats.md", "w")
+f.write("###### Stats for Today\n" + df_today.to_markdown() + "\n\n###### Averages\n" + df_avg.to_markdown())
+f.close()
