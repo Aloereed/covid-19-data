d1797466c2a2965eec9c1378f0f348ea48d1eb09
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-14 22:07:48 -0500

    update

--- daily_stats.py
+++ daily_stats.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import os
 import datetime
-
-def prepend_line(file_name, line):
-    dummy_file = file_name + '.bak'
-    with open(file_name, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
-        write_obj.write(line + '\n')
-        for line in read_obj:
-            write_obj.write(line)
-    os.remove(file_name)
-    os.rename(dummy_file, file_name)
+import pandas as pd 
+from tabulate import tabulate
 
 cases_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_cases_daily.csv', dtype='int32')
 deaths_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_daily.csv', dtype='int32')
@@ -19,13 +11,20 @@ deaths_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_
 cases_array = np.array(cases_daily_csv)
 cases_length = len(cases_array)
 cases_today = cases_array[cases_length - 1]
+cases_mean = np.mean(cases_array)
 
 deaths_array = np.array(deaths_daily_csv)
 deaths_length = len(deaths_array)
 deaths_today = deaths_array[deaths_length - 1]
+deaths_mean = np.mean(deaths_array)
 
 today = datetime.datetime.today().strftime('%m-%d-%Y')
+index_today = ['Cases', 'Deaths']
+df_today = pd.DataFrame({today: [cases_today, deaths_today]}, index=index_today) 
 
-line = '# US COVID-19 Data\n## ' + str(today) + ' Statistics\n#### <pre> +--------------+---------------+ </pre>\n#### <pre> | Cases Today  |  Deaths Today | </pre>\n#### <pre> | ' + str(cases_today) + '        | ' + " " + str(deaths_today) + '         | </pre>\n#### <pre> +--------------+---------------+ </pre>'
+index_avg = ['Cases per day', 'Deaths per day']
+df_avg = pd.DataFrame({'avg': [int(cases_mean), int(deaths_mean)]}, index=index_avg)
+df_avg.index.names = ['2021-01-21 - ' + today]
 
-prepend_line("README.md", line)
+print(tabulate(df_today, headers = 'keys', tablefmt = 'psql'))
+print(tabulate(df_avg, headers = 'keys', tablefmt = 'psql'))
