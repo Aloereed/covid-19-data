88f810d5dcf4b830d93f645a415bb4cd6c6d0cdd
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-16 09:05:39 -0500

    Updating data.

--- plot_data.py
+++ plot_data.py
@@ -4,11 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_data():
-# Plot Total Cases
+    # csv files needed for arrays
     dates_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_dates.csv', dtype='datetime64')
     cases_total_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_cases_total.csv', dtype='int32')
+    deaths_total_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_total.csv', dtype='int32')
+    cases_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_cases_daily.csv', dtype='int32')
+    deaths_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_daily.csv', dtype='int32')
 
+    # X axis for all plots
     x = np.array(dates_csv)
+
+    # Plot Total Cases
     y = np.array((cases_total_csv) / 1000000)
 
     plt.figure('US Total COVID-19 Cases', figsize=(15, 8))
@@ -20,11 +26,7 @@ def plot_data():
     plt.savefig("/home/nuke/git/covid-19-data/plots/US_Total_COVID-19_Cases.png")
     plt.show()
 
-# Plot Total Deaths
-    dates_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_dates.csv', dtype='datetime64')
-    deaths_total_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_total.csv', dtype='int32')
-
-    x = np.array(dates_csv)
+    # Plot Total Deaths
     y = np.array((deaths_total_csv) / 1000)
 
     plt.figure('US Total COVID-19 Deaths', figsize=(15, 8))
@@ -36,34 +38,26 @@ def plot_data():
     plt.savefig("/home/nuke/git/covid-19-data/plots/US_Total_COVID-19_Deaths.png")
     plt.show()
 
-# Plot Daily Cases
-    dates_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_dates.csv', dtype='datetime64')
-    cases_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_cases_daily.csv', dtype='int32')
-
-    x = np.array(dates_csv)
-    z = np.array((cases_daily_csv) / 1000)
+    # Plot Daily Cases
+    y = np.array((cases_daily_csv) / 1000)
 
     plt.figure('US Daily COVID-19 Cases', figsize=(15, 8))
     plt.title("US Daily COVID-19 Cases")
     plt.ylabel("Cases (in thousands)")
     plt.grid(True, ls = "-.")
-    plt.yticks(np.arange(min(z), max(z) + 100, 50))
-    plt.plot(x, z, color = "b")
+    plt.yticks(np.arange(min(y), max(y) + 100, 50))
+    plt.plot(x, y, color = "b")
     plt.savefig("/home/nuke/git/covid-19-data/plots/US_Daily_COVID-19_Cases.png")
     plt.show()
 
-# Plot Daily Deaths
-    dates_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_dates.csv', dtype='datetime64')
-    deaths_daily_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_daily.csv', dtype='int32')
-
-    x = np.array(dates_csv)
-    z = np.array(deaths_daily_csv)
+    # Plot Daily Deaths
+    y = np.array(deaths_daily_csv)
 
     plt.figure('US Daily COVID-19 Deaths', figsize=(15, 8))
     plt.title("US Daily COVID-19 Deaths")
     plt.ylabel("Deaths")
     plt.grid(True, ls = "-.")
-    plt.yticks(np.arange(min(z), max(z) + 1000, 500))
-    plt.plot(x, z, color = "b")
+    plt.yticks(np.arange(min(y), max(y) + 1000, 500))
+    plt.plot(x, y, color = "b")
     plt.savefig("/home/nuke/git/covid-19-data/plots/US_Daily_COVID-19_Deaths.png")
     plt.show()
