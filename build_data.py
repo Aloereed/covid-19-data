475a0a810d59d5730c22cd7b9c98f8538ad18121
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-17 16:02:09 -0500

    Updating data.

--- build_data.py
+++ build_data.py
@@ -40,24 +40,12 @@ def build_data():
     np.savetxt('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_daily.csv', deaths_daily, fmt='%d')
     np.savetxt('/home/nuke/git/covid-19-data/data/us_covid-19_cases_daily.csv', cases_daily, fmt='%d')
 
-    # Create csv for daily data
-    df_daily = pd.read_csv('/home/nuke/git/covid-19-data/data/us_covid-19_dates.csv', header=0, names=['date'])
-    cases_daily = pd.read_csv('/home/nuke/git/covid-19-data/data/us_covid-19_cases_daily.csv', header=None, dtype=str)      
+    # csv files needed to aggregate all data
+    df = pd.read_csv('/home/nuke/git/covid-19-data/us.csv', header=0, names=['date', 'total cases', 'total deaths'])
+    cases_daily = pd.read_csv('/home/nuke/git/covid-19-data/data/us_covid-19_cases_daily.csv', header=None, dtype=str)
     deaths_daily = pd.read_csv('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_daily.csv', header=None, dtype=str)
-    df_daily["daily cases"] = cases_daily
-    df_daily["daily deaths"] = deaths_daily
-    df_daily.to_csv("/home/nuke/git/covid-19-data/data/us_covid-19_daily.csv", index=False)
-"""
-    # Create csv for total data                                                                                                 
-    cases_total = pd.read_csv('/home/nuke/git/covid-19-data/data/us_covid-19_cases_total.csv', header=None, dtype=str)      
-    deaths_total = pd.read_csv('/home/nuke/git/covid-19-data/data/us_covid-19_deaths_total.csv', header=None, dtype=str)
-    df["total cases"] = cases_total
-    df["total deaths"] = deaths_total
-    df.to_csv("/home/nuke/git/covid-19-data/data/us_covid-19_total.csv", index=False)
 
-    # Create csv for all aggregated data
-    df = pd.read_csv('/home/nuke/git/covid-19-data/data/us_covid-19_total.csv', header=0, names=['date', 'total cases', 'total deaths'])
+    # Create aggregated csv
     df["daily cases"] = cases_daily
     df["daily deaths"] = deaths_daily
     df.to_csv("/home/nuke/git/covid-19-data/data/us_covid-19_data.csv", index=False)
-"""
