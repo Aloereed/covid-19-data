b7f85b62454a67e4fe5746f6c2abfcc4364d7955
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-17 19:00:54 -0500

    Updating data.

--- README.md
+++ README.md
@@ -7,10 +7,10 @@
 | New deaths | 1,707        |
 
 ###### Averages
-| 2020-01-21 - 2021-02-16   | avg    |
-|:--------------------------|:-------|
-| Cases per day             | 70,745 |
-| Deaths per day            | 1,241  |
+| 2021-02-10 - 2021-02-16   | 7-day avg   |
+|:--------------------------|:------------|
+| Cases                     | 81,199      |
+| Deaths                    | 2,821       |
 
 
 ## [Total Cases and Deaths](https://github.com/drebrb/covid-19-data/blob/master/data/us_covid-19_total.csv)
--- write_to_readme.py
+++ write_to_readme.py
@@ -9,33 +9,36 @@ def write_to_readme():
     new_deaths_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_new_deaths.csv', dtype='int32')
     dates_csv = np.genfromtxt('/home/nuke/git/covid-19-data/data/us_covid-19_dates.csv', dtype='datetime64')
 
-    # Build array for new cases and mean to date
+    # Build array for new cases and 7-day mean
     cases_array = np.array(new_cases_csv)
     cases_length = len(cases_array)
     cases_today = cases_array[cases_length - 1]
-    cases_mean = np.mean(cases_array)
+    cases_last_7_days = cases_array[-7:]
+    cases_mean = np.mean(cases_last_7_days)
 
-    # Build array for new deaths and mean to date
+    # Build array for new deaths and 7-day mean
     deaths_array = np.array(new_deaths_csv)
     deaths_length = len(deaths_array)
     deaths_today = deaths_array[deaths_length - 1]
-    deaths_mean = np.mean(deaths_array)
+    deaths_last_7_days = deaths_array[-7:]
+    deaths_mean = np.mean(deaths_last_7_days)
 
-    # Most recent date reported
+    # Dates 
     date_array = np.array(dates_csv)
     date_length = len(date_array)
     date_today = date_array[date_length - 1 ]
- 
+    date_7_days_ago = date_array[date_length - 7 ]
+
     # Dataframe for cases and deaths in the last 24 hours
     today = date_today
     index_today = ['New cases', 'New deaths']
     df_today = pd.DataFrame({str(today): [f"{cases_today:,d}", f"{deaths_today:,d}"]}, index=index_today)
     df_today.index.names = ['Date']
 
-    # DataFrame for averages to date
-    index_avg = ['Cases per day', 'Deaths per day']
-    df_avg = pd.DataFrame({'avg': [f"{int(cases_mean):,d}", f"{int(deaths_mean):,d}"]}, index=index_avg)
-    df_avg.index.names = ['2020-01-21 - ' + str(today)]
+    # DataFrame for 7-day average
+    index_avg = ['Cases', 'Deaths']
+    df_avg = pd.DataFrame({'7-day avg': [f"{int(cases_mean):,d}", f"{int(deaths_mean):,d}"]}, index=index_avg)
+    df_avg.index.names = [str(date_7_days_ago) + " - " + str(today)]
 
     # Write to 'README.md'
     f = open("/home/nuke/git/covid-19-data/README.md", "w")
