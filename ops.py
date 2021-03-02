49a4599c984f7e69c24692c0291fbf3ad2ea92f4
drebrb <andre.jbarbosa@pm.me>
2021-03-01 23:58:33 -0500

    Updating data.

--- ops.py
+++ ops.py
@@ -5,35 +5,6 @@ def mk_dir(*dirs):
             print(f"creating '{os.path.join(os.getcwd(), d)}'")
             os.mkdir(d)
 
-def timeout(first, i):
-    import numpy as np
-    from tqdm import tqdm
-    from time import sleep
-    delta = np.timedelta64(10, 's')
-    last = first + delta
-    time = np.arange(first, last)
-    time = np.array(time)
-    time = tqdm(time, ncols=80)
-    for t in time:
-        n = np.datetime64('now')
-        S = (n - first).astype('int64')
-        s = S
-        if s > 60:
-            s = s // 60 + i
-            i += 1
-        if i > 60:
-            i = 0
-        m = S // 60
-        h = m // 60
-        d = h // 24
-        s = str(s).zfill(2)
-        m = str(m).zfill(2)
-        h = str(h).zfill(2)
-        d = str(d).zfill(2)
-        uptime = f"uptime: {d} {h}:{m}:{s}"
-        time.set_description(f"{uptime}")
-        sleep(1)
-
 def fetch(url, start_time):
     import requests, hashlib, os, tempfile
     from time import sleep
@@ -52,7 +23,30 @@ def fetch(url, start_time):
         fp = os.path.join(tempfile.gettempdir(), hashlib.sha256(digest.encode('utf-8')).hexdigest())
         if os.path.isfile(fp) and os.stat(fp).st_size > 0:
             print("no update available")
-            timeout(start_time, i)
+            delta = np.timedelta64(10, 's')
+            last = first + delta
+            time = np.arange(first, last)
+            time = np.array(time)
+            time = tqdm(time, ncols=80)
+            for t in time:
+                n = np.datetime64('now')
+                S = (n - start_time).astype('int64')
+                s = S
+                if s > 60:
+                    s = s // 60 + i
+                    i += 1
+                if i > 60:
+                    i = 0
+                m = S // 60
+                h = m // 60
+                d = h // 24
+                s = str(s).zfill(2)
+                m = str(m).zfill(2)
+                h = str(h).zfill(2)
+                d = str(d).zfill(2)
+                uptime = f"uptime: {d} {h}:{m}:{s}"
+                time.set_description(f"{uptime}")
+                sleep(1)
         else:
             print(f"writing to '{fp}'")
             with open(f"{fp}.tmp", 'wb') as f:
