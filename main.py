63e0292b797d265043d00dce3162607923540d74
drebrb <andre.jbarbosa@pm.me>
2021-03-04 17:50:33 -0500

    Updating data.

--- main.py
+++ main.py
@@ -1,128 +1,84 @@
 #!/usr/bin/env python3
-import time
+import requests, hashlib, os, tempfile, io
+from time import time, sleep
 from tqdm import trange
-from time import sleep
-import os, requests, hashlib, tempfile, io
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from README_TEMPLATE import README_TEMPLATE
 from subprocess import check_call
 
-# start time
-st = time.time()
+st = time()
 
-# create directories if they dont exist
-def mk_dir(*dirs):
-    for d in dirs:
-        if not os.path.isdir(d):
-            print(f"creating '{os.path.join(os.getcwd(), d)}'")
-            os.mkdir(d)
+def fetch(url): 
+    z = 1
+    while True:
+        try:
+            print(f"fetching '{url}'")
+            with requests.get(url, stream=True) as response:
+                response.raise_for_status()
+                dat = response.content
+            print("comparing hashes")
+            sig = hashlib.md5()
+            for line in response.iter_lines():
+                sig.update(line)
+            digest = sig.hexdigest()
+            fp = os.path.join(tempfile.gettempdir(), hashlib.md5(digest.encode('utf-8')).hexdigest())
+            if os.path.isfile(fp) and os.stat(fp).st_size > 0:
+                print("no update available")
+                timeout(3600)
+                z //= z
+            else:
+                print(f"writing to '{fp}'")
+                with open(f"{fp}.tmp", 'wb') as f:
+                    f.write(dat)
+                os.rename(f"{fp}.tmp", fp)
+                return dat
+        except Exception as error:
+            retry(z, error)
+            z += 1
 
-mk_dir('data', 'plots')
+def timeout(s):
+    timeout = trange(s, ncols=103, leave=False, ascii=' #')
+    for t in timeout:
+        timeout.set_description(uptime())
+        sleep(1)
 
-class Run:    # fetch and update data
-    def __init__(self, rt=10):
-        self.rt = rt
+def uptime(): 
+    ct = time()
+    et = ct - st
+    d = (et // 86400) % 365
+    h = (et // 3600) % 24
+    m = (et // 60) % 60
+    s = et % 60
+    d = int(d) 
+    h = int(h)
+    m = int(m)
+    s = int(s)
+    d = str(d).zfill(3)
+    h = str(h).zfill(2)
+    m = str(m).zfill(2)
+    s = str(s).zfill(2)
+    uptime = f"uptime: {d} {h}:{m}:{s}"
+    return uptime
 
-    def fetch(self, url): 
-        while True:
-            try:
-                print(f"fetching '{url}'")
-                with requests.get(url, stream=True) as response:
-                    response.raise_for_status()
-                    dat = response.content
-                print("comparing hashes")
-                sig = hashlib.md5()
-                for line in response.iter_lines():
-                    sig.update(line)
-                digest = sig.hexdigest()
-                fp = os.path.join(tempfile.gettempdir(), hashlib.md5(digest.encode('utf-8')).hexdigest())
-                if os.path.isfile(fp) and os.stat(fp).st_size > 0:
-                    print("no update available")
-                    self.timeout(3600)
-                else:
-                    print(f"writing to '{fp}'")
-                    with open(f"{fp}.tmp", 'wb') as f:
-                        f.write(dat)
-                    os.rename(f"{fp}.tmp", fp)
-                    return dat
-            except Exception as error:
-                self.retry(error) 
-
-    # time between fetches
-    def timeout(self, sec):
-        timeout = trange(sec, ncols=103, leave=False, ascii=' #')
-        for t in timeout:
-            timeout.set_description(self.uptime())
-            sleep(1)
-
-    # total time program has been online
-    def uptime(self): 
-        ct = time.time()
-        et = ct - st
-        d = (et // 86400) % 365
-        h = (et // 3600) % 24
-        m = (et // 60) % 60
-        s = et % 60
-        d = int(d) 
-        h = int(h)
-        m = int(m)
-        s = int(s)
-        d = str(d).zfill(3)
-        h = str(h).zfill(2)
-        m = str(m).zfill(2)
-        s = str(s).zfill(2)
-        uptime = f"uptime: {d} {h}:{m}:{s}"
-        return uptime
-
-    # retry if exception occured
-    def retry(self, error):
-        print(f"\n{error}\n")
-        self.rt -= 1
-        if self.rt > 0:
-            self.timeout(10)
-        else:
-            print("max retries exceeded")
-            exit(1)
-    
-    # update readme 
-    def write_readme(self, template, date, df_24, df_avg):
-        print(f"writing to '{os.path.join(os.getcwd(), 'README.md')}'")
-        with open('README.md', 'w') as f:
-            f.write(template.format(date, df_24, df_avg))
-
-    # update github repo
-    def push_git(self):
-        if os.path.isdir('.git'):
-            try:
-                check_call('/usr/bin/git add .', shell=True)
-                check_call('/usr/bin/git commit -m "Updating data."', shell=True)
-            except Exception as error:
-                print(f"\n{error}\n")
-            while True:
-                try:
-                    check_call('/usr/bin/git push', shell=True)
-                    break
-                except Exception as error:
-                    self.retry(error)
-
-r = Run()
-
-# **** build data ****
+def retry(z, error):
+    print(f"\n{str(z).zfill(2)}/10: {error}\n")
+    if z < 10:
+        timeout(6)
+    else:
+        print("max retries exceeded")
+        exit(1)
 
 while True:
 
-    # get data
-    dat = r.fetch('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv')
+    dat = fetch('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv')
    
-    # parse data and build arrays
     df = pd.read_csv(io.StringIO(dat.decode('utf-8')))
     dates = np.array(df['date'], dtype='datetime64')
     total_cases = np.array(df['cases'], dtype='int64')
     total_deaths = np.array(df['deaths'], dtype='int64')
 
-    # calculate new cases and deaths
     def get_diff(arr):
         arr = np.asarray(arr)
         diff = np.diff(arr)
@@ -132,31 +88,32 @@ while True:
     new_cases = get_diff(total_cases)
     new_deaths = get_diff(total_deaths)
 
-    # **** create csv's ****
+    def mk_dir(*dirs):
+        for d in dirs:
+            if not os.path.isdir(d):
+                print(f"creating '{os.path.join(os.getcwd(), d)}'")
+                os.mkdir(d)
+
+    mk_dir('data', 'plots')
+
     print(f"writing to '{os.path.join(os.getcwd(), 'data')}'")
 
-    # create csv for total cases and deaths
     df = pd.DataFrame({'date': dates, 'total cases': total_cases,
         'total deaths': total_deaths})
     df.to_csv('data/us_covid-19_total.csv', index=False)
 
-    # create csv for new cases and deaths
     df = pd.DataFrame({'date': dates, 'new cases': new_cases, 
         'new deaths': new_deaths})
     df.to_csv('data/us_covid-19_new.csv', index=False)
 
-    # create csv for all aggregated data
     df = pd.DataFrame({'date': dates, 'total cases': total_cases, 
         'total deaths': total_deaths, 'new cases': new_cases, 'new deaths': new_deaths})
     df.to_csv('data/us_covid-19_data.csv', index=False)
 
-    # **** plot data ****
     print(f"writing to '{os.path.join(os.getcwd(), 'plots')}'")
 
-    # x axis for all plots
     x = dates
 
-    # plot total cases
     y = total_cases / 1000000
     plt.figure('US Total COVID-19 Cases', figsize=(15, 8))
     plt.title('US Total COVID-19 Cases')
@@ -166,7 +123,6 @@ while True:
     plt.plot(x, y, color='b')
     plt.savefig('plots/US_Total_COVID-19_Cases.png')
 
-    # plot total deaths
     y = total_deaths / 1000
     plt.figure('US Total COVID-19 Deaths', figsize=(15, 8))
     plt.title('US Total COVID-19 Deaths')
@@ -176,7 +132,6 @@ while True:
     plt.plot(x, y, color='b')
     plt.savefig('plots/US_Total_COVID-19_Deaths.png')
 
-    # plot new cases
     y = new_cases / 1000
     plt.figure('US New COVID-19 Cases', figsize=(15, 8))
     plt.title('US New COVID-19 Cases')
@@ -186,7 +141,6 @@ while True:
     plt.plot(x, y, color='b')
     plt.savefig('plots/US_New_COVID-19_Cases.png')
 
-    # plot new deaths
     y = new_deaths
     plt.figure('US New COVID-19 Deaths', figsize=(15, 8))
     plt.title('US New COVID-19 Deaths')
@@ -196,32 +150,43 @@ while True:
     plt.plot(x, y, color='b')
     plt.savefig('plots/US_New_COVID-19_Deaths.png')
 
-    # **** write to README.md ****
-
-    # new cases and deaths in the last 24 hours
     cases = new_cases[-1]
     deaths = new_deaths[-1]
 
-    # 7-day mean for new cases and deaths
     cmean = np.mean(new_cases[-7:])
     dmean = np.mean(new_deaths[-7:])
 
-    # date
     date = dates[-1]
 
-    # DataFrame for new cases and deaths in the last 24 hours 
     df_24 = pd.DataFrame({'New cases': [f'{cases:,d}'], 'New deaths': [f'{deaths:,d}']})
     df_24 = df_24.to_markdown(index=False, disable_numparse=True)
 
-    # DataFrame for 7-day average
     df_avg = pd.DataFrame({'Cases': [f'{int(cmean):,d}'], 'Deaths': [f'{int(dmean):,d}']})
     df_avg = df_avg.to_markdown(index=False, disable_numparse=True)
 
-    # write to 'README.md'
-    r.write_readme(README_TEMPLATE(), date, df_24, df_avg)
+    def write_readme(template, date, df_24, df_avg):
+        print(f"writing to '{os.path.join(os.getcwd(), 'README.md')}'")
+        with open('README.md', 'w') as f:
+            f.write(template.format(date, df_24, df_avg))
 
-    # **** push to github ****
-    r.push_git() 
+    write_readme(README_TEMPLATE(), date, df_24, df_avg)
 
-    # **** timeout ****
-    r.timeout(3600)
+    def push_git():
+        if os.path.isdir('.git'):
+            try:
+                check_call('/usr/bin/git add .', shell=True)
+                check_call('/usr/bin/git commit -m "Updating data."', shell=True)
+            except Exception as error:
+                print(f"\n{error}\n")
+            z = 1
+            while True:
+                try:
+                    check_call('/usr/bin/git push', shell=True)
+                    break
+                except Exception as error:
+                    retry(z, error)
+                    z += 1
+
+    push_git() 
+
+    timeout(3600)
