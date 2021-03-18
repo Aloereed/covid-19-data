e19e6025cb8d804e01e07d138c4816ebd60eed7c
drebrb <andre.jbarbosa@pm.me>
2021-03-17 20:24:45 -0400

    Updating data.

--- README_TEMPLATE.py
+++ README_TEMPLATE.py
@@ -5,5 +5,8 @@ def README_TEMPLATE():
 ###### [ [U.S. Data](us.csv) ([Raw CSV](https://raw.githubusercontent.com/drebrb/covid-19-data/master/us.csv)) | [U.S. State-Level Data](states) ]
 {}
 ###### [ [U.S. Plot](us.png) ]
-![Plot](https://github.com/drebrb/covid-19-data/blob/master/us.png)''')
+![Plot](https://github.com/drebrb/covid-19-data/blob/master/us.png)
+# US COVID-19 Vaccinations
+###### [ [U.S. Vaccinations](us_vaccinations.csv) ([Raw CSV](https://raw.githubusercontent.com/drebrb/covid-19-data/master/vaccinations/us_vaccinations.csv)) | [U.S. State-Level ](vaccinations/states) ]
+![Plot](https://github.com/drebrb/covid-19-data/blob/master/vaccinations/us_vaccinations.png)''')
     return README_TEMPLATE
